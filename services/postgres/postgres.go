72b4c659490f45bfda49ef70f9832f64dde12a28
Veld <velddev@gmail.com>
2019-06-01 01:23:11 +0200

    Add: migrations app

--- services/postgres/postgres.go
+++ services/postgres/postgres.go
@@ -19,17 +19,57 @@ func InitDB(connStr string) {
 }
 
 // RunMigrations runs migrations for the database.
-func RunMigrations(connStr string, untilTime int64) {
+func RunMigrations(connStr string, untilTime int64) error {
 	if Db == nil {
 		InitDB(connStr);
 	}
+
+	rows, err := Db.Query(`SELECT EXISTS (
+		SELECT 1
+		FROM   information_schema.tables 
+		WHERE  table_schema = 'public'
+		AND    table_name = '_databasemigrations'
+		);`)
+	if err != nil {
+		return err
+	}
+	
+	var exists bool
+	if rows.Next() {
+		rows.Scan(&exists)
+	}
+	if !exists {
+		_, err = Db.Query(`CREATE TABLE public._databasemigrations (
+			id BIGINT PRIMARY KEY);`)
+		if err != nil {
+			return err
+		}
+	}
+
 	for _, migration := range []MigrationEntry{ 
 		(*migrations.Initial)(nil), 
 	} {
+		// If migration exists, skip.
+		rows, err := Db.Query(`SELECT 1 FROM public._databasemigrations WHERE id = $1`,
+			migration.Id())
+		if rows.Next() {
+			continue;
+		}
+
 		if(migration.Id() >= untilTime) {
-			migration.Up(Db)
+			err = migration.Up(Db)
+			if err != nil {
+				return err
+			}
+
+			_, err = Db.Query(`INSERT INTO public._databasemigrations (id) VALUES ($1);`,
+				migration.Id())
+			if err != nil {
+				return err
+			}
 		}
 	}
+	return nil
 }
 
 // MigrationEntry is used as a base interface for migrations
